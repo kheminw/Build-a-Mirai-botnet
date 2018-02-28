3be48abcdd4455c36ab3d0013c332a4e15340079
A.z fan <kulukami@gmail.com>
2018-02-28 20:07:24 +0800

    Update cross-compile.sh

--- scripts/cross-compile.sh
+++ scripts/cross-compile.sh
@@ -97,6 +97,5 @@ export PATH=$PATH:/etc/xcompile/m68k/bin
 export PATH=$PATH:/etc/xcompile/mips/bin
 export PATH=$PATH:/etc/xcompile/mipsel/bin
 export PATH=$PATH:/etc/xcompile/powerpc/bin
-export PATH=$PATH:/etc/xcompile/powerpc-440fp/bin
 export PATH=$PATH:/etc/xcompile/sh4/bin
 export PATH=$PATH:/etc/xcompile/sparc/bin
